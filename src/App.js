b478c7323e245fb0a6451ae335ecbfe06f11530e
Dammy <bestbynature2010@gmail.com>
2023-05-19 12:08:57 +0100

    :rocket: Update the routings

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Italy from './components/Italy';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>REACT APP</h2>
+      <Header />
+      <Routes>
+        <Route path="/country" element={<Layout />} />
+        <Route index element={<Home />} />
+        <Route path="/country/italy" element={<Italy />} />
+        <Route path="/country/germany" element={<Italy />} />
+        <Route path="/country/poland" element={<Italy />} />
+        <Route path="/country/nigeria" element={<Italy />} />
+        <Route path="/country/ghana" element={<Italy />} />
+        <Route path="/country/spain" element={<Italy />} />
+        <Route path="/country/united kingdom" element={<Italy />} />
+        <Route path="/country/czeck republic" element={<Italy />} />
+      </Routes>
     </div>
   );
 }
